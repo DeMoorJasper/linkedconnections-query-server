8ff6fbc9302de924fc36b9135d9a5f24c2e9cc0b
Jasper De Moor <jasperdemoor@gmail.com>
2018-06-08 17:01:49 +0200

    fix tests

--- test/cache.js
+++ test/cache.js
@@ -5,7 +5,10 @@ const LDFetch = require('ldfetch');
 describe('Cache', function() {
   it('Should cache an uncached request', async function() {
     let cache = new CacheFetcher(new LDFetch());
+    let requestUri = `http://belgium.linkedconnections.org/sncb/connections/?departureTime=2018-06-08T14%3A22%3A25.374Z`;
+    await cache.get(requestUri);
+    let cachedFile = await cache.readCache(requestUri);
 
-    cache.get('http://belgium.linkedconnections.org/sncb/connections/?departureTime=2018-06-08T14%3A22%3A25.374Z');
+    assert(cachedFile !== null);
   });
 });
